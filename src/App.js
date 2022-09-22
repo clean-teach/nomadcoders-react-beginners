340f849ae643ee05188a58b8a1e74cf9385fcefd
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-09-22 20:40:00 +0900

    #6.2 Deps
    ## 문제
    
    - 두 개 이상의 State가 있을때, 하나의 State만 수정이 됐는데 수정되지 않은 나머지 State가 리렌더링이 된다.
        - 예 : input의 `onChange` Event의 경우, 키보드를 한번 클릭 할때마다 컴포넌트 전체가 재실행 된다.
    
    ## 목표
    
    - 수정된 State만을 리렌더링 하고자 한다.
    
    ## `Deps` 로 해결
    
    - `React.useEffect(effect, [state])`
        - `Deps`의 array에 넣어준 State가 수정 될 때에만 해당 코드가 실행된다.
        - `Deps`의 array가 비어 있는 경우에는 수정 될 상태가 없기 때문에 해당 코드가 한번만 실행된다.
    - `useEffect` 는 특정 코드를 원하는 때(특정 State가 수정 될 때)에 실행 할 수 있다.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [count, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => {
     setValue(prev => prev + 1);
   };
+  const onChange = event => {
+    setKeyword(event.target.value);
+  }
   console.log('I run All the Time');
   useEffect(() => console.log('I run only Once'), []);
+  useEffect(() => console.log('I run when "conut" change'), [count]);
+  useEffect(() => {
+    if(keyword !== '' && keyword.length > 5){
+      console.log('I run when "keyword" change')
+    }
+  }, [keyword]);
+  useEffect(() => console.log('I run when "conut & keyword" change'), [count, keyword]);
+
   return (
     <div >
       <h1 className={styles.title}>{count}</h1>
       <button onClick={onClick}>click me</button>
+      <hr />
+      <input type="text" onChange={onChange} value={keyword} />
     </div>
   );
 }
