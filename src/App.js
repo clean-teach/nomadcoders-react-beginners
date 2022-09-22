2cd30938862463f1500f70f45f23522074cb3a39
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-09-22 19:51:38 +0900

    #6.1 useEffect
    # Introduction
    
    ## Effects 의 필요
    
    1. State가 수정될 때 컴포넌트의 모든 코드들은 다시 실행된다.
    2. 어떠한 경우(API 요청 등)에는 State가 수정되더라도 특정 코드를 처음 한번만 실행하고자 할 때가 있을 것이다.
    
    # `useEffect`
    
    - `React.useEffect(effect, Deps)`
        - 첫 번째 argument : 한번만 실행하고자 하는 코드
        - 두 번째 argument : `Deps`

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Button from "./Button";
+// import Button from "./Button";
 import styles from './App.module.css'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setValue] = useState(0);
+  const onClick = () => {
+    setValue(prev => prev + 1);
+  };
+  console.log('I run All the Time');
+  useEffect(() => console.log('I run only Once'), []);
   return (
     <div >
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={'Continue'} />
+      <h1 className={styles.title}>{count}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
