9f82c71bb21b0c2b038c227da5b2bcd3bab0fe1c
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-09-24 03:25:00 +0900

    #6.4 Cleanup
    - Component 가 사라질 때 실행 되는 함수
    - `useEffect()` 함수 내에서 함수를 return
    - 자주 쓰이는 문법은 아니지만, 컴포넌트가 사라질 때의 분석 결과를 확인한다던가 등의 이유로 사용 할 수 있는 문법

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 // import Button from "./Button";
-import styles from './App.module.css'
 import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => {
-    setValue(prev => prev + 1);
-  };
-  const onChange = event => {
-    setKeyword(event.target.value);
-  }
-  console.log('I run All the Time');
-  useEffect(() => console.log('I run only Once'), []);
-  useEffect(() => console.log('I run when "conut" change'), [count]);
+function Hello(){
   useEffect(() => {
-    if(keyword !== '' && keyword.length > 5){
-      console.log('I run when "keyword" change')
-    }
-  }, [keyword]);
-  useEffect(() => console.log('I run when "conut & keyword" change'), [count, keyword]);
+    console.log("hi :)");
+    return () => console.log("cleanup function");
+  }, [])
+  return (
+    <h1>Hello</h1>
+  )
+}
+function App() {
+  const [shoing, setShoing] = useState(false);
+  const onClick = () => {
+    setShoing(!shoing);
+  };
 
   return (
     <div >
-      <h1 className={styles.title}>{count}</h1>
-      <button onClick={onClick}>click me</button>
-      <hr />
-      <input type="text" onChange={onChange} value={keyword} />
+      {shoing ? <Hello /> : null}
+      <button onClick={onClick}>{shoing ? 'hide' : 'show'}</button>
     </div>
   );
 }
